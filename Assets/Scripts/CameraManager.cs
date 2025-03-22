570b85bcbb2401159acf483594626f5dd9a6bc85
Toothless <hyunjinney@naver.com>
2025-03-22 11:50:32 +0900

    refactor. PlayManager, CameraManager 분리
    - 카메라 기능 담당 스크립트 따로 분리

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,16 +1,191 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
-public class CameraManager : MonoBehaviour
+public class CameraManager : Singleton<CameraManager>
 {
+    public GameObject mainCamera;
+
+    public bool isFirstPerson = false; //1인칭 모드 여부
+    private bool isRotateAroundPlayer = true; //카메라가 플레이어 주위를 회전하는지 여부 
+
+
+    public float thirdPersonDistance = 3.0f; //3인칭모드에서 플레이어와 카메라의 거리
+    public Vector3 thirdPersonOffset = new Vector3(0, 1.5f, 0f);//3인칭 모드에서 카메라 오프셋
+    public Transform playerLookObj; //플레이어 시야 위치
+
+    public float zoomDistance = 1.0f;//카메라가 확대될 때의 거리 - 3인칭 모드
+    public float zoomSpeed = 5.0f; //확대 축소가 되는 속도
+    public float defaultFov = 60.0f; //기본 카메라 시야 각
+    public float zoomFov = 30.0f;// 확대 시 카메라 시야각 - 1인칭 모드
+
+    private float currentDistance; //현재 카메라와의 거리 - 3인칭 모드
+    private float targetDistance; //목표 카메라 거리
+    private float targetFov; //목표 FOv
+    private bool isZoomed = false; //확대 여부 확인
+    private Coroutine zoomCoroutine; // 코루틴을 사용하여 확대축소 처리
+    public MultiAimConstraint multiAimConstraint; //처음시작할 때 틀어져있게 돼서 코드로 수정
+    public GameObject crosshairObj;
+
+
+    private PlayerManager pm;
+
+    public Transform aimTarget;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mainCamera.GetComponent<Camera>().fieldOfView = defaultFov;
+        currentDistance = thirdPersonDistance;
+        targetDistance = thirdPersonDistance;
+        targetFov = defaultFov;
+
+        pm = PlayerManager.Instance;
+
+        crosshairObj.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CameraSet();
     }
+
+    void CameraSet()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            isFirstPerson = !isFirstPerson;
+            Debug.Log(isFirstPerson ? "1인칭 모드" : "3인칭 모드");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isRotateAroundPlayer = !isRotateAroundPlayer;
+            Debug.Log(isRotateAroundPlayer ? "카메라가 주위를 회전합니다." : "플레이어가 시야를 따라서 회전합니다.");
+        }
+        ZoomCamera();
+    }
+
+    void ZoomCamera()
+    {
+        if (pm.isWeaponGet && pm.isWeponHold)
+        {
+            if (Input.GetMouseButtonDown(1)) //조준해서 줌하기
+            {
+                if(pm.isCrouching)
+                {
+                    pm.characterController.height = 1.3f;
+                }
+
+                pm.isAim = true;
+                multiAimConstraint.data.offset = new Vector3(-30, 0, 0);
+                pm.animator.SetLayerWeight(1, 1); //첫번째 레이어 값을 1로 바꿔라(활성화)
+                crosshairObj.SetActive(true);
+                if (zoomCoroutine != null)
+                {
+                    StopCoroutine(zoomCoroutine); //실행중인 코루틴 있으면 멈춤
+                }
+                if (isFirstPerson) //1인칭일 때
+                {
+                    SetTargetFOV(zoomFov); //타겟 거리를 1인칭 줌 거리로 설정
+                    zoomCoroutine = StartCoroutine(ZoomFieldOfView(targetFov)); //1인칭일 때 카메라 줌 코루틴 실행
+                }
+                else //3인칭 일때 
+                {
+                    SetTargetDistance(zoomDistance); //타겟 거리를 줌 거리로 설정
+                    zoomCoroutine = StartCoroutine(ZoomCamera(targetDistance)); //3인칭일 때 카메라 줌 코루틴 실행 
+                }
+
+            }
+            if (Input.GetMouseButtonUp(1)) //돌아오기 
+            {
+                pm.characterController.height = 1.9f;
+                if (zoomCoroutine != null)
+                {
+                    StopCoroutine(zoomCoroutine); //실행중인 코루틴 있으면 멈춤
+                }
+                if (isFirstPerson) //1인칭일 때
+                {
+                    SetTargetFOV(defaultFov); //타겟 거리를 1인칭 줌 거리로 설정
+                    zoomCoroutine = StartCoroutine(ZoomFieldOfView(targetFov)); //1인칭일 때 카메라 줌 코루틴 실행
+                }
+                else //3인칭 일때 
+                {
+                    SetTargetDistance(thirdPersonDistance); //타겟 거리를 줌 거리로 설정
+                    zoomCoroutine = StartCoroutine(ZoomCamera(targetDistance)); //3인칭일 때 카메라 줌 코루틴 실행 
+                }
+                pm.isAim = false;
+                multiAimConstraint.data.offset = new Vector3(0, 0, 0);
+                pm.animator.SetLayerWeight(1, 0);
+                crosshairObj.SetActive(false);
+            }
+        }
+
+    }
+
+    public void UpdateCameraPosition()
+    {
+        if (isRotateAroundPlayer)
+        {
+            //카메라가 플레이어 오른쪽에서 회전하도록 설정
+            Vector3 direction = new Vector3(0, 0, -currentDistance);
+            Quaternion rotation = Quaternion.Euler(pm.pitch, pm.yaw, 0);
+
+            //카메라를 플레이어의 오른쪽에서 고정된 위치로 이동
+            mainCamera.transform.position = pm.gameObject.transform.position + thirdPersonOffset + rotation * direction;
+
+            //카메라가 플레이어의 위치를 따라가도록 설정(플레이어를 바라보는 것이 아니라 살짝 옆을 보게)
+            mainCamera.transform.LookAt(pm.gameObject.transform.position + new Vector3(thirdPersonOffset.x, thirdPersonOffset.y, 0));
+        }
+        else //플레이어가 직접 도는 
+        {
+            pm.gameObject.transform.rotation = Quaternion.Euler(0f, pm.yaw, 0);
+            Vector3 direction = new Vector3(0, 0, -currentDistance);
+
+            mainCamera.transform.position = playerLookObj.position + thirdPersonOffset + Quaternion.Euler(pm.pitch, pm.yaw, 0) * direction;
+            mainCamera.transform.LookAt(playerLookObj.position + new Vector3(0, thirdPersonOffset.y, 0));
+
+            UpdateAimTarget();
+
+        }
+    }
+    void UpdateAimTarget()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        aimTarget.position = ray.GetPoint(10.0f);
+    }
+
+    public void SetTargetDistance(float distance)
+    {
+        targetDistance = distance;
+    }
+    public void SetTargetFOV(float fov)
+    {
+        targetFov = fov;
+    }
+
+    IEnumerator ZoomCamera(float targetDistance) //3인칭일 때 카메라 위치를 조정
+    {
+        while (Mathf.Abs(currentDistance - targetDistance) > 0.01f) //현재 거리에서 목표거리로 부드럽게 이동
+        {
+            currentDistance = Mathf.Lerp(currentDistance, targetDistance, Time.deltaTime * zoomSpeed);
+            yield return null;
+        }
+
+        currentDistance = targetDistance; //목표거리 도달 후 값 고정
+    }
+
+    IEnumerator ZoomFieldOfView(float targetFov) //1인칭일 때(카메라 자체가 움직이는 게 아니라 줌만 되면 되니까)
+    {
+        while (Mathf.Abs(mainCamera.GetComponent<Camera>().fieldOfView - targetFov) > 0.01f)
+        {
+            mainCamera.GetComponent<Camera>().fieldOfView = Mathf.Lerp(mainCamera.GetComponent<Camera>().fieldOfView, targetFov, Time.deltaTime * zoomSpeed);
+            yield return null;
+        }
+        mainCamera.GetComponent<Camera>().fieldOfView = targetFov;
+    }
+
+
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,9 +1,7 @@
 //using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Animations.Rigging; //nameSpace : 소속
 using UnityEngine.UI;
-using static UnityEngine.UI.Image;
 
 public enum WeaponMode
 {
@@ -13,33 +11,17 @@ public enum WeaponMode
 
 public class PlayerManager : Singleton<PlayerManager>
 {
+    public Transform playerHead;//플레이어 머리 위치 - 1인칭 모드를 위해
+
     public HPScriptableObject hpManager;
 
     public float moveSpeed = 5.0f; //플레이어 이동 속도
     public float mouseSensitivity = 100.0f;//마우스 감도
-    public Transform cameraTransform; //카메라 트랜스폼
     public CharacterController characterController; // 캐릭터 컨트롤러 컴포넌트
-    public Transform playerHead;//플레이어 머리 위치 - 1인칭 모드를 위해
-    public float thirdPersonDistance = 3.0f; //3인칭모드에서 플레이어와 카메라의 거리
-    public Vector3 thirdPersonOffset = new Vector3(0, 1.5f, 0f);//3인칭 모드에서 카메라 오프셋
-    public Transform playerLookObj; //플레이어 시야 위치
+    private Transform mainCamera; //카메라 컴포넌트
 
-    public float zoomDistance = 1.0f;//카메라가 확대될 때의 거리 - 3인칭 모드
-    public float zoomSpeed = 5.0f; //확대 축소가 되는 속도
-    public float defaultFov = 60.0f; //기본 카메라 시야 각
-    public float zoomFov = 30.0f;// 확대 시 카메라 시야각 - 1인칭 모드
-
-    private float currentDistance; //현재 카메라와의 거리 - 3인칭 모드
-    private float targetDistance; //목표 카메라 거리
-    private float targetFov; //목표 FOv
-    private bool isZoomed = false; //확대 여부 확인
-    private Coroutine zoomCoroutine; // 코루틴을 사용하여 확대축소 처리
-    private Camera mainCamera; //카메라 컴포넌트
-
-    private float pitch = 0.0f; //위아래 회전값
-    private float yaw = 0.0f; // 좌우 회전값
-    private bool isFirstPerson = false; //1인칭 모드 여부
-    private bool isRotateAroundPlayer = true; //카메라가 플레이어 주위를 회전하는지 여부 
+    public float pitch = 0.0f; //위아래 회전값
+    public float yaw = 0.0f; // 좌우 회전값
 
     //중력 관련 변수
     public float gravity = -9.81f;
@@ -47,41 +29,38 @@ public class PlayerManager : Singleton<PlayerManager>
     private Vector3 velocity;
     private bool isGround;
 
-    private Animator animator;
+    public Animator animator;
     float horizontal;
     float vertical;
     private bool isRunning = false;
-    private bool isCrouching = false;
+    public bool isCrouching = false;
     public float walkSpeed = 5.0f;
     public float runSpeed = 10.0f;
     public float crouchSpeed = 3.0f;
 
-    private bool isAim = false;
+    public bool isAim = false;
     private bool isFire = false;
 
     public GameObject shotGunObj;
 
-    private int animationSpeed = 1;
-    private string currentAnimation = "Idle";
-    //private bool isPickingUP = false;
-    private AnimatorStateInfo animatorStateInfo;
+    // private int animationSpeed = 1;
+    // private string currentAnimation = "Idle";
+    // //private bool isPickingUP = false;
+    // private AnimatorStateInfo animatorStateInfo;
 
-    public Transform aimTarget;
     private float weaponMaxDistance = 100f;
 
     public LayerMask TargetLayerMask;
 
-    public MultiAimConstraint multiAimConstraint; //처음시작할 때 틀어져있게 돼서 코드로 수정
 
     public Vector3 boxSize = new Vector3(2.0f, 2.0f, 2.0f);
     public float castDistance = 5.0f;
     public LayerMask itemLayer;
     public Transform itemGetPos; //raycast 가 어디서 나타날지 정해주는
 
-    public GameObject crosshairObj;
-    private bool isWeaponGet = false;
+    public bool isWeaponGet = false;
     public GameObject weaponIcon;
-    private bool isWeponHold = false;
+    public bool isWeponHold = false;
 
     //public ParticleSystem shotGunEffect;
     public GameObject shotGunPoint;
@@ -102,7 +81,6 @@ public class PlayerManager : Singleton<PlayerManager>
 
     public GameObject PauseObj;
     private bool isPause = false;
-    public AudioClip audioClipPause;
 
     public GameObject handPos;
 
@@ -112,19 +90,12 @@ public class PlayerManager : Singleton<PlayerManager>
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        currentDistance = thirdPersonDistance;
-        targetDistance = thirdPersonDistance;
-        targetFov = defaultFov;
-        mainCamera = cameraTransform.GetComponent<Camera>();
-        mainCamera.fieldOfView = defaultFov;
+        mainCamera = CameraManager.Instance.mainCamera.transform;
         animator = GetComponent<Animator>();
         shotGunObj.SetActive(false);
 
-        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
-
         animator.SetFloat("Horizontal", horizontal);
 
-        crosshairObj.SetActive(false);
         weaponIcon.SetActive(false);
 
         bulletText.gameObject.SetActive(false);
@@ -140,7 +111,6 @@ public class PlayerManager : Singleton<PlayerManager>
     void Update()
     {
         MouseSet();
-        CameraSet();
         WeaponChange();
         Attack();
 
@@ -257,11 +227,6 @@ public class PlayerManager : Singleton<PlayerManager>
 
     }
 
-    void UpdateAimTarget()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        aimTarget.position = ray.GetPoint(10.0f);
-    }
 
     void DebugBox(Vector3 origin, Vector3 direction)
     {
@@ -422,7 +387,7 @@ public class PlayerManager : Singleton<PlayerManager>
         }
 
 
-        if (isFirstPerson)
+        if (CameraManager.Instance.isFirstPerson)
         {
             FirstPersonMovement();
         }
@@ -444,92 +409,17 @@ public class PlayerManager : Singleton<PlayerManager>
 
     }
 
-    void CameraSet()
-    {
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            isFirstPerson = !isFirstPerson;
-            Debug.Log(isFirstPerson ? "1인칭 모드" : "3인칭 모드");
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            isRotateAroundPlayer = !isRotateAroundPlayer;
-            Debug.Log(isRotateAroundPlayer ? "카메라가 주위를 회전합니다." : "플레이어가 시야를 따라서 회전합니다.");
-        }
-        ZoomCamera();
-    }
-
-    void ZoomCamera()
-    {
-        if (isWeaponGet && isWeponHold)
-        {
-            if (Input.GetMouseButtonDown(1)) //조준해서 줌하기
-            {
-                if(isCrouching)
-                {
-                    characterController.height = 1.3f;
-                }
-
-                isAim = true;
-                multiAimConstraint.data.offset = new Vector3(-30, 0, 0);
-                animator.SetLayerWeight(1, 1); //첫번째 레이어 값을 1로 바꿔라(활성화)
-                crosshairObj.SetActive(true);
-                if (zoomCoroutine != null)
-                {
-                    StopCoroutine(zoomCoroutine); //실행중인 코루틴 있으면 멈춤
-                }
-                if (isFirstPerson) //1인칭일 때
-                {
-                    SetTargetFOV(zoomFov); //타겟 거리를 1인칭 줌 거리로 설정
-                    zoomCoroutine = StartCoroutine(ZoomFieldOfView(targetFov)); //1인칭일 때 카메라 줌 코루틴 실행
-                }
-                else //3인칭 일때 
-                {
-                    SetTargetDistance(zoomDistance); //타겟 거리를 줌 거리로 설정
-                    zoomCoroutine = StartCoroutine(ZoomCamera(targetDistance)); //3인칭일 때 카메라 줌 코루틴 실행 
-                }
-
-            }
-            if (Input.GetMouseButtonUp(1)) //돌아오기 
-            {
-                characterController.height = 1.9f;
-                if (zoomCoroutine != null)
-                {
-                    StopCoroutine(zoomCoroutine); //실행중인 코루틴 있으면 멈춤
-                }
-                if (isFirstPerson) //1인칭일 때
-                {
-                    SetTargetFOV(defaultFov); //타겟 거리를 1인칭 줌 거리로 설정
-                    zoomCoroutine = StartCoroutine(ZoomFieldOfView(targetFov)); //1인칭일 때 카메라 줌 코루틴 실행
-                }
-                else //3인칭 일때 
-                {
-                    SetTargetDistance(thirdPersonDistance); //타겟 거리를 줌 거리로 설정
-                    zoomCoroutine = StartCoroutine(ZoomCamera(targetDistance)); //3인칭일 때 카메라 줌 코루틴 실행 
-                }
-                isAim = false;
-                multiAimConstraint.data.offset = new Vector3(0, 0, 0);
-                animator.SetLayerWeight(1, 0);
-                crosshairObj.SetActive(false);
-            }
-        }
-
-    }
     void WeaponChange()
     {
         if (isWeaponGet)
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                //audioSource.PlayOneShot(audioClipWeaponChange);
                 animator.SetTrigger("isWeaponChange");
                 shotGunObj.SetActive(true);
                 isWeponHold = true;
             }
         }
-        
-        //animator.SetBool("isAim", isAim);
     }
     int cnt = 0;
     void Attack()
@@ -543,7 +433,6 @@ public class PlayerManager : Singleton<PlayerManager>
             }
             else
             {
-                //재장전
                 //총알이 없는 소리 재생 및 리턴
                 SoundManager.Instance.PlaySFX("EmpthyTrigger", transform.position);
                 return;
@@ -578,16 +467,9 @@ public class PlayerManager : Singleton<PlayerManager>
                 if(hitObject.CompareTag("Enemy"))
                 {
                     ParticleManager.Instance.PlayParticle(ParticleType.Explosion, hit.point);
-                    /*ParticleSystem particle = Instantiate(DamageParticleSystem, hit.point,Quaternion.identity);
-
-                    particle.Play();
-                    //audioSource.PlayOneShot(audioClipDamage);*/
                     StartCoroutine(hitObject.GetComponent<ZombieManager>()?.TakeDamage(attackPower));
-                    
                 }
-                
                 Debug.DrawLine(ray.origin, hit.point, Color.red, 2.0f);
-                    
             }
             else
             {
@@ -609,17 +491,17 @@ public class PlayerManager : Singleton<PlayerManager>
         vertical = Input.GetAxis("Vertical");
 
         //카메라가 바라보는 방향 
-        Vector3 moveDirection = cameraTransform.forward * vertical + cameraTransform.right * horizontal;
+        Vector3 moveDirection = mainCamera.forward * vertical + mainCamera.right * horizontal;
         moveDirection.y = 0.0f; //y출 방향은 0
         characterController.Move(moveDirection * moveSpeed * Time.deltaTime); // 그 방향으로 movespeed만큼 속력으로 이동
 
 
         //카메라 위치를 플레이어 머리로 
-        cameraTransform.position = playerHead.position;
+        mainCamera.position = playerHead.position;
         //1인칭에서 카메라 바라보는 각도를 마우스 각도에 따라서 변경
-        cameraTransform.rotation = Quaternion.Euler(pitch, yaw, 0.0f);
+        mainCamera.rotation = Quaternion.Euler(pitch, yaw, 0.0f);
         //플레이어를 카메라 y각도랑 일치시킴
-        transform.rotation = Quaternion.Euler(0f, cameraTransform.eulerAngles.y, 0.0f);
+        transform.rotation = Quaternion.Euler(0f, mainCamera.eulerAngles.y, 0.0f);
     }
 
     void ThirdPersonMovemnet()
@@ -631,77 +513,9 @@ public class PlayerManager : Singleton<PlayerManager>
         characterController.Move(move * moveSpeed * Time.deltaTime);
 
 
-        UpdateCameraPosition();
+        CameraManager.Instance.UpdateCameraPosition();
     }
-
-    void UpdateCameraPosition()
-    {
-        if (isRotateAroundPlayer)
-        {
-            //카메라가 플레이어 오른쪽에서 회전하도록 설정
-            Vector3 direction = new Vector3(0, 0, -currentDistance);
-            Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
-
-            //카메라를 플레이어의 오른쪽에서 고정된 위치로 이동
-            cameraTransform.position = transform.position + thirdPersonOffset + rotation * direction;
-
-            //카메라가 플레이어의 위치를 따라가도록 설정(플레이어를 바라보는 것이 아니라 살짝 옆을 보게)
-            cameraTransform.LookAt(transform.position + new Vector3(thirdPersonOffset.x, thirdPersonOffset.y, 0));
-        }
-        else //플레이어가 직접 도는 
-        {
-            transform.rotation = Quaternion.Euler(0f, yaw, 0);
-            Vector3 direction = new Vector3(0, 0, -currentDistance);
-
-            cameraTransform.position = playerLookObj.position + thirdPersonOffset + Quaternion.Euler(pitch, yaw, 0) * direction;
-            cameraTransform.LookAt(playerLookObj.position + new Vector3(0, thirdPersonOffset.y, 0));
-
-            UpdateAimTarget();
-
-        }
-    }
-
-    public void SetTargetDistance(float distance)
-    {
-        targetDistance = distance;
-    }
-    public void SetTargetFOV(float fov)
-    {
-        targetFov = fov;
-    }
-
-    IEnumerator ZoomCamera(float targetDistance) //3인칭일 때 카메라 위치를 조정
-    {
-        while (Mathf.Abs(currentDistance - targetDistance) > 0.01f) //현재 거리에서 목표거리로 부드럽게 이동
-        {
-            currentDistance = Mathf.Lerp(currentDistance, targetDistance, Time.deltaTime * zoomSpeed);
-            yield return null;
-        }
-
-        currentDistance = targetDistance; //목표거리 도달 후 값 고정
-    }
-
-    IEnumerator ZoomFieldOfView(float targetFov) //1인칭일 때(카메라 자체가 움직이는 게 아니라 줌만 되면 되니까)
-    {
-        while (Mathf.Abs(mainCamera.fieldOfView - targetFov) > 0.01f)
-        {
-            mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, targetFov, Time.deltaTime * zoomSpeed);
-            yield return null;
-        }
-        mainCamera.fieldOfView = targetFov;
-    }
-
-    public void WeaponChangeSoundOn()
-    {
-        SoundManager.Instance.PlaySFX("ItemPickUP", transform.position);
-    }
-
-    public void ShootingGunSoundOn()
-    {
-        SoundManager.Instance.PlaySFX("ShotGunShoot", transform.position);
-        ParticleManager.Instance.PlayParticle(ParticleType.WeaponFire, shotGunPoint.transform.position);
-        //shotGunEffect.Play();
-    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("PlayerDamage"))
@@ -722,5 +536,17 @@ public class PlayerManager : Singleton<PlayerManager>
     private void ChangeCharacterHeight()
     {
         characterController.height = 1.3f;
+    }
+
+    public void WeaponChangeSoundOn()
+    {
+        SoundManager.Instance.PlaySFX("ItemPickUP", transform.position);
+    }
+
+    public void ShootingGunSoundOn()
+    {
+        SoundManager.Instance.PlaySFX("ShotGunShoot", transform.position);
+        ParticleManager.Instance.PlayParticle(ParticleType.WeaponFire, shotGunPoint.transform.position);
+        //shotGunEffect.Play();
     }
 }
